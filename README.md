ded49c606d78b5de7a6c3f851182560c2bb5baab
Christopher Greaves <28542943+Chris-Greaves@users.noreply.github.com>
2021-10-16 23:18:34 +0100

    Add downloading and installing of Fonts

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 This is a repo containing scripts and configs for setting up PowerShell and Terminals exactly how I like them.
 
+## Usage
+
+You will need to run the `install.ps1` script as an administrator.
+
 ## Stuff to add
 
 - Automatically add Nerd Fonts so Oh My Posh themes can work
--- install.ps1
+++ install.ps1
@@ -11,21 +11,55 @@ param (
     # Skip Installing Oh My Posh
     [Parameter()]
     [switch]
-    $skipOhMyPosh
+    $skipOhMyPosh,
+    # Skip Installing Nerd Font
+    [Parameter()]
+    [switch]
+    $skipNerdFont
 )
 
 if (!$skipTerminalIcons) {
+    echo "Installing Icons"
     Install-Module -Name Terminal-Icons -Repository PSGallery   
 }
 
 if (!$skipPSReadLine) {
+    echo "Installing PSReadLine"
     Install-Module PSReadLine -AllowPrerelease -AllowClobber -Force
 }
 
 if (!$skipOhMyPosh) {
+    echo "Installing Oh My Posh"
     winget install JanDeDobbeleer.OhMyPosh
 }
 
+if (!$skipNerdFont) {
+    echo "Installing Nerd fonts"
+    $dloadLink = "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip"
+    $tempFolder = "$env:temp\PowerShell-Setup"
+    $tempFontZip = "$tempFolder\fonts.zip"
+
+    md -Force "$tempFolder\fonts"
+
+    echo "Downloading fonts"
+    Invoke-WebRequest -Uri $dloadLink -OutFile $tempFontZip
+    Expand-Archive -Path $tempFontZip -DestinationPath "$tempFolder\fonts"
+
+    echo "Installing each ttf font"
+    $fonts = (New-Object -ComObject Shell.Application).Namespace(0x14)
+    foreach ($file in gci "$tempFolder\fonts\*.ttf")
+    {
+        $fileName = $file.Name
+        if (-not(Test-Path -Path "C:\Windows\fonts\$fileName" )) {
+            echo $fileName
+            dir $file | %{ $fonts.CopyHere($_.fullname) }
+        }
+    }
+    cp "$tempFolder\fonts\*.ttf" C:\windows\fonts\
+}
+
+echo "Copying PowerShell profile"
 Copy-Item ".\Microsoft.PowerShell_profile.ps1" -Destination $profile
 
+echo "Copying Oh My Posh theme"
 Copy-Item ".\blueier.omp.json" -Destination $HOME
